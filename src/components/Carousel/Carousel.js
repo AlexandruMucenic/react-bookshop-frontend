3cb9652fc1530f59dcf1ac3d4bed9437f13b94db
Alexandru Mucenic <alexmucenic6@gmail.com>
2022-11-17 18:54:07 +0200

    changed carousel images

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,50 +1,53 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./Carousel.css";
-import CarouselOne from "../../images/carousel/CarouselOne.webp";
-import CarouselTwo from "../../images/carousel/CarouselTwo.webp";
+import React, { useEffect, useRef, useState } from 'react'
+import './Carousel.css'
+import CarouselOne from '../../images/carousel/CarouselOne.jpg'
+import CarouselTwo from '../../images/carousel/CarouselTwo.jpg'
+import CarouselThree from '../../images/carousel/CarouselThree.jpg'
 
 const Carousel = () => {
-  const carouselImages = [CarouselOne, CarouselTwo];
-  const [index, setIndex] = useState(0);
-  const delay = 2500;
-  const timeoutRef = useRef(null);
+  const carouselImages = [CarouselOne, CarouselTwo, CarouselThree]
+  const [index, setIndex] = useState(0)
+  const delay = 2500
+  const timeoutRef = useRef(null)
 
   function resetTimeout() {
     if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
+      clearTimeout(timeoutRef.current)
     }
   }
 
   useEffect(() => {
-    resetTimeout();
+    resetTimeout()
     timeoutRef.current = setTimeout(
       () => setIndex(prevIndex => (prevIndex === carouselImages.length - 1 ? 0 : prevIndex + 1)),
       delay
-    );
+    )
     return () => {
-      resetTimeout();
-    };
-  }, [index]);
+      resetTimeout()
+    }
+  }, [index])
 
-  return <div className="slideshow">
-    <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
-      {carouselImages.map((image, index) => (
-        <div className="slide" key={index} style={{ backgroundImage: `url(${image})` }}></div>
-      ))}
+  return (
+    <div className="slideshow">
+      <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+        {carouselImages.map((image, index) => (
+          <div className="slide" key={index} style={{ backgroundImage: `url(${image})` }}></div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {carouselImages.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? ' active' : ''}`}
+            onClick={() => {
+              setIndex(idx)
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
+  )
+}
 
-    <div className="slideshowDots">
-      {carouselImages.map((_, idx) => (
-        <div
-          key={idx}
-          className={`slideshowDot${index === idx ? " active" : ""}`}
-          onClick={() => {
-            setIndex(idx);
-          }}>
-        </div>
-      ))}
-    </div>
-  </div>;
-};
-
-export default Carousel;
+export default Carousel
